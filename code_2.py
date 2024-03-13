f76ff8a24ebe791fe2ae9957827f936cca30e0de
Simone <144659113+Simonesho@users.noreply.github.com>
2024-03-13 22:25:00 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,17 @@
+
+function main() {
+}
+    });
+        data.push(Math.floor(Math.random() * 100) + 1);
+    return data;
+    data.forEach(item => {
+    const data = [];
+        console.log("Random Number: " + item);
+
+}
+    const data = generateRandomData();
+    }
+    for (let i = 0; i < 10; i++) {
+main();
+
+function generateRandomData() {
